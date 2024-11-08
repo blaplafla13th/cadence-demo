e5a9b1ff1fb042f82aa3a2f3b6a57f5dc1980fec
phambathang <42430845+blaplafla13th@users.noreply.github.com>
2024-11-08 08:38:50 +0700

    init

--- main.sh
+++ main.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-micromamba install bioconda::bcftools bioconda::tabix
+micromamba install bioconda::bcftools
+sudo apt install -y tabix
 bash gip.sh -data_path . -data_name chr6_327k.all.0.25.missing -num_al 3 -num_gpus 1
